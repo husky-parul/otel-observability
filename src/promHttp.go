efcb9833ef7b0494b274edf45cc9eb21ddf967be
Parul Singh <parsingh@redhat.com>
2023-10-31 07:58:31 +0100

    promt+otelp
    Signed-off-by: Parul Singh <parsingh@redhat.com>

--- src/promHttp.go
+++ src/promHttp.go
@@ -12,7 +12,9 @@ import (
 	runtimemetrics "go.opentelemetry.io/contrib/instrumentation/runtime"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/prometheus"
-	"go.opentelemetry.io/otel/metric"
+	metric "go.opentelemetry.io/otel/metric"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 )
 
@@ -42,7 +44,26 @@ func configureMetrics() *prometheus.Exporter {
 	return exporter
 }
 
-func main() {
+func configureOtlpHttp() {
+	ctx := context.Background()
+	exp, err := otlpmetrichttp.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	meterProvider := sdkmetric.NewMeterProvider(sdkmetric.WithReader(sdkmetric.NewPeriodicReader(exp)))
+	defer func() {
+		if err := meterProvider.Shutdown(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	otel.SetMeterProvider(meterProvider)
+
+	// From here, the meterProvider can be used by instrumentation to collect
+	// telemetry.
+}
+
+func main__() {
 	ctx := context.Background()
 	configureOpentelemetry()
 
